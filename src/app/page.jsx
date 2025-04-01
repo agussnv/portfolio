61687ae7313aabdf8b3b0ace130693e7097cad7a
a23agunovnov <a23agunovnov@inspedralbes.cat>
2025-04-01 08:31:46 +0200

    Estructuración del proyecto

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="max-w-7xl">
+        <div className="border border-black rounded-xl"></div>
+      </div>
+    </>
+  );
+}
